ce84c6a2d5f27d09196c8fbc4f5208f53de4b538
Joshua Price <joshuapricew@gmail.com>
2020-11-19 11:12:49 +0000

    Create Room class and add GenerateRoomID function

---
+++ WeWatch/RoomClass.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace WeWatch
+{
+    public class Room
+    {
+        /// <summary>
+        /// Generates and returns a unique 4 character base64 string.
+        /// </summary>
+        /// <returns>4 character base64 string</returns>
+        // TODO: Add parameter which takes list of all RoomIds currently
+        // being tracked by the program so that this class can check that 
+        // it is generating a fully unique Id for this server.
+        static public string GenerateRoomID()
+        {
+            string RoomId;
+            do {
+
+                byte[] RoomGuidBytes = Guid.NewGuid().ToByteArray();
+                // Only get a number of bits that is a common multiple of both 6 and 8.
+                byte[] RoomIdBytes = new byte[]
+                {
+                    RoomGuidBytes[0],
+                    RoomGuidBytes[1],
+                    RoomGuidBytes[2]
+                };
+                RoomId = Convert.ToBase64String(RoomGuidBytes);
+                /* If the base64 contains either a + or a /, try again. */
+            } while (Regex.IsMatch(RoomId, "[+/]"));
+            return RoomId;
+        }
+    }
+}
