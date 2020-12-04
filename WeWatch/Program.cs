4d859f7a696d074a8a67bd749caf1beffc19f8d2
Joshua Price <joshuapricew@gmail.com>
2020-12-04 10:12:30 +0000

    Listen to all incoming connections

--- WeWatch/Program.cs
+++ WeWatch/Program.cs
@@ -21,7 +21,7 @@ namespace WeWatch
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:5000;");
+                    webBuilder.UseUrls("http://*:5000;");
                 });
     }
 }
